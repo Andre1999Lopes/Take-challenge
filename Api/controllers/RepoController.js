b29bf1ad6ef37777b825d2f4003c02a068b3ed18
André <andrejuniorlopes@gmail.com>
2021-08-11 12:59:24 -0300

    Definido o retorno correto para uso no BLiP

--- Api/controllers/RepoController.js
+++ Api/controllers/RepoController.js
@@ -2,21 +2,19 @@ const fetch = require('node-fetch');
 
 class RepoController {
     static async getRepos() {
-        try {
-            const url = 'https://api.github.com/orgs/takenet/repos?type=all&direction=asc&per_page=5';
-            const res = await fetch(url, {
-            	headers: {
-            		accept: 'application/vnd.github.v3+json'
-            	}
-            })
-            .then(res => res.json())
-    	    .then(data => {return data;});
-    	    
-    	    return res;
-        }
-        catch (error) {
-            return error;
-        }
+        const url = 'https://api.github.com/orgs/takenet/repos?type=all&direction=asc&per_page=5';
+        const res = await fetch(url, {
+            headers: {
+                accept: 'application/vnd.github.v3+json'
+            }
+        })
+        .then(res => res.json())
+        .then(data => {return data;});
+        
+        return res;
+    }
+    catch (error) {
+        return error;
     }
 }
 
--- Api/index.js
+++ Api/index.js
@@ -1,16 +1,27 @@
 const RepoController = require('./controllers/RepoController.js');
 
 exports.handler = async (event) => {
-    const response = await RepoController.getRepos();
+    try {
+        const response = await RepoController.getRepos();
         
-    const repos = new Array();
-    
-    response.forEach(repo => {
-    	repos.push({
-    		name: repo.name,
-    		description: repo.description
-    	});
-    });
-    
-    return repos;
+        const repos = new Array();
+        
+        response.forEach(repo => {
+            repos.push({
+                name: repo.name,
+                description: repo.description
+            });
+        });
+        
+        return {
+            statusCode: 200,
+            body: repos
+        };
+    }
+    catch (error) {
+        return {
+            statusCode: 500,
+            message: "Erro interno"
+        }
+    }
 }
